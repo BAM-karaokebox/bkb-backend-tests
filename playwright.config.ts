f0bafe9feee749626a2f24481eb62dcc03fb4bfe
Tomas Veschambre <tomas.vesch@gmail.com>
2022-04-25 11:02:01 +0200

    BAM-986 Remove unnecessary comments

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,10 +4,7 @@ import { devices } from '@playwright/test';
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
+/*
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
@@ -17,7 +14,6 @@ const config: PlaywrightTestConfig = {
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
      */
     timeout: 5000,
   },
@@ -27,16 +23,14 @@ const config: PlaywrightTestConfig = {
   retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  /* Reporter to use. */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  /* Shared settings for all the projects below. */
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    /* Maximum time each action such as `click()` can take. */
     actionTimeout: 6000,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    /* Collect trace when retrying the failed test. Check at the end of the report */
     trace: 'retain-on-failure',
   },
 
@@ -49,15 +43,6 @@ const config: PlaywrightTestConfig = {
       },
     },
   ],
-
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
 };
 
 export default config;
