17a2ee479a52fefa331a3c80e5f4433002c236c5
TomasVeschambre <76953729+TomasVeschambre@users.noreply.github.com>
2022-05-02 10:56:51 +0200

    Update playwright.config.ts
    Modify timeout of each test from 210000 to 240000

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 210000,
+  timeout: 240000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
