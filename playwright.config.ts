80a3d7695ebffd1d0647634e23ce24a0d25f8410
Laurent Malvert <laurent.malvert.freelance@gmail.com>
2022-05-19 21:12:12 +0200

    BAM-1036 update timeouts + add retry of 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,12 +14,16 @@ const config: PlaywrightTestConfig = {
     /**
      * Maximum time expect() should wait for the condition to be met.
      */
-    timeout: 10000,
+    timeout: 15000,
   },
+  headless: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* We don't want any retries on the backend, for performance reasons */
-  retries: 0,
+  /*
+   * We allow one retry to make the tests less prone to time-out issues,
+   * but no more (to avoid aggravating ongoing performance crunches).
+   */
+  retries: 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. */
@@ -27,7 +31,7 @@ const config: PlaywrightTestConfig = {
   /* Shared settings for all the projects below. */
   use: {
     /* Maximum time each action such as `click()` can take. */
-    actionTimeout: 10000,
+    actionTimeout: 15000,
 
     /* Collect trace when a test fail. Check at the end of the report */
     trace: 'retain-on-failure',
